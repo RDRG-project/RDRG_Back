13395f1f2156d17270e28aa7cb4b3c90c7bc65bb
kymial <nisijamaren@gmail.com>
2024-05-20 16:25:06 +0900

    디바이스 추가

---
+++ back/src/main/java/com/rdrg/back/service/DeviceService.java
@@ -0,0 +1,19 @@
+package com.rdrg.back.service;
+
+import org.springframework.http.ResponseEntity;
+
+import com.rdrg.back.dto.request.device.PostDeviceRequestDto;
+import com.rdrg.back.dto.response.ResponseDto;
+import com.rdrg.back.dto.response.device.GetDeviceListResponseDto;
+
+public interface DeviceService {
+
+    ResponseEntity<ResponseDto> postDevice(PostDeviceRequestDto dto, String serialNumber);
+
+    ResponseEntity<? super GetDeviceListResponseDto> getDeviceList();
+
+    ResponseEntity<ResponseDto> deleteDevice(String serialNumber);
+
+
+    
+}
