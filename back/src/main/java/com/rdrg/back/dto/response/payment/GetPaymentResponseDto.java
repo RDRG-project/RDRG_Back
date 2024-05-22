8cc5a5c18ca806f75424106242ce01b8463b4d27
Chixol <phw9278@gmail.com>
2024-05-22 16:13:45 +0900

    결제 내역 불러오기 API

---
+++ back/src/main/java/com/rdrg/back/dto/response/payment/GetPaymentResponseDto.java
@@ -0,0 +1,36 @@
+package com.rdrg.back.dto.response.payment;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.rdrg.back.dto.response.ResponseCode;
+import com.rdrg.back.dto.response.ResponseDto;
+import com.rdrg.back.dto.response.ResponseMessage;
+import com.rdrg.back.entity.DeviceRentStatusEntity;
+
+import lombok.Getter;
+
+@Getter
+public class GetPaymentResponseDto extends ResponseDto {
+    
+    private String userId;
+    private String rentalPeriod;
+    private String rentPlace;
+    private Integer rentTotalPrice;
+
+    private GetPaymentResponseDto(DeviceRentStatusEntity deviceRentStatusEntity) throws Exception {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        String totalRentalPeriod = deviceRentStatusEntity.getRentDatetime() + "~" + deviceRentStatusEntity.getRentReturnDatetime();
+
+        this.userId = deviceRentStatusEntity.getRentUserId();
+        this.rentalPeriod = totalRentalPeriod;
+        this.rentPlace = deviceRentStatusEntity.getRentPlace();
+        this.rentTotalPrice = deviceRentStatusEntity.getRentTotalPrice();
+    }
+
+    public static ResponseEntity<GetPaymentResponseDto> success(DeviceRentStatusEntity deviceRentStatusEntity) throws Exception {
+        GetPaymentResponseDto responseBody = new GetPaymentResponseDto(deviceRentStatusEntity);
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+
+}
--- back/src/main/java/com/rdrg/back/repository/PaymentRepository.java
+++ back/src/main/java/com/rdrg/back/repository/PaymentRepository.java
@@ -3,10 +3,14 @@ package com.rdrg.back.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.rdrg.back.entity.DeviceRentStatusEntity;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<DeviceRentStatusEntity, Integer> {
     
     List<DeviceRentStatusEntity> findByOrderByRentNumberDesc();
+
+    DeviceRentStatusEntity findTop1ByRentUserIdOrderByRentNumberDesc(String rentUserId);
 }
