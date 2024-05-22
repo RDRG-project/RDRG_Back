4dd0a2cb0cb218f6e6d794afd200205e0705b4e9
Chixol <phw9278@gmail.com>
2024-05-22 17:37:53 +0900

    나의 대여 내역불러오기 api

---
+++ back/src/main/java/com/rdrg/back/common/object/RentItem.java
@@ -0,0 +1,34 @@
+package com.rdrg.back.common.object;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.rdrg.back.entity.DeviceRentStatusEntity;
+
+import lombok.Getter;
+
+@Getter
+public class RentItem {
+    
+    private String rentDatetime;
+    private String rentReturnDatetime;
+    private boolean rentStatus;
+
+    private RentItem(DeviceRentStatusEntity deviceRentStatusEntity, boolean rentStatus) throws Exception {
+
+        this.rentDatetime = deviceRentStatusEntity.getRentDatetime();
+        this.rentReturnDatetime = deviceRentStatusEntity.getRentReturnDatetime();
+        this.rentStatus = rentStatus;
+    }
+
+    public static List<RentItem> getRentList(List<DeviceRentStatusEntity> deviceRentStatusEntities, boolean rentStatus) throws Exception {
+        List<RentItem> rentItems = new ArrayList<>();
+
+        for (DeviceRentStatusEntity deviceRentStatusEntity : deviceRentStatusEntities) {
+            RentItem rentItem = new RentItem(deviceRentStatusEntity, rentStatus);
+            rentItems.add(rentItem);
+        }
+
+        return rentItems;
+    }
+}
