e68e2217f681fcaa38ea9e5bf31ce95ac4d58dff
Chixol <phw9278@gmail.com>
2024-05-23 15:22:09 +0900

    예약 가능한 리스트 불러오기

--- back/src/main/java/com/rdrg/back/repository/DeviceRepository.java
+++ back/src/main/java/com/rdrg/back/repository/DeviceRepository.java
@@ -1,9 +1,12 @@
 package com.rdrg.back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.rdrg.back.entity.DeviceEntity;
+
 import java.util.List;
 
 @Repository
@@ -15,4 +18,12 @@ public interface DeviceRepository extends JpaRepository<DeviceEntity, String> {
     DeviceEntity findBySerialNumber(String serialNumber);
 
     List<DeviceEntity> findByOrderBySerialNumber();
+
+    @Query("SELECT d FROM Device d WHERE d.serialNumber NOT IN (" +
+        "  SELECT drs.rentSerialNumber FROM DeviceRentStatus drs WHERE " +
+        "    (drs.rentDatetime < :inputRentDatetime AND drs.rentReturnDatetime > :inputReturnDatetime) OR " +
+        "    (drs.rentDatetime < :inputReturnDatetime AND drs.rentReturnDatetime > :inputReturnDatetime) OR " +
+        "    (drs.rentDatetime > :inputRentDatetime AND drs.rentReturnDatetime < :inputReturnDatetime))")
+    List<DeviceEntity> findAvailableDevices(@Param("inputRentDatetime") String inputRentDatetime,
+        @Param("inputReturnDatetime") String inputReturnDatetime);
 }
