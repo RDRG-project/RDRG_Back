8ac2182bb5c585fd33c96d858ef823d09bc03391
Chixol <phw9278@gmail.com>
2024-05-24 20:53:21 +0900

    게시물 불러오기 수정(url값 같이 불러옴)

--- back/src/main/java/com/rdrg/back/repository/BoardRepository.java
+++ back/src/main/java/com/rdrg/back/repository/BoardRepository.java
@@ -3,9 +3,11 @@ package com.rdrg.back.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.rdrg.back.entity.BoardEntity;
 
+@Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
     List<BoardEntity> findByOrderByReceptionNumberDesc();
     List<BoardEntity> findByTitleContainsOrderByReceptionNumberDesc(String title);
