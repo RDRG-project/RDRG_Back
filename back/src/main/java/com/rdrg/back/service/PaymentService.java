df5123035c654a3440e359e14b3e6c3acb6b52d3
Chixol <phw9278@gmail.com>
2024-06-10 11:42:24 +0900

    대여 상태 변경 api 작성

--- back/src/main/java/com/rdrg/back/service/PaymentService.java
+++ back/src/main/java/com/rdrg/back/service/PaymentService.java
@@ -2,6 +2,7 @@ package com.rdrg.back.service;
 
 import org.springframework.http.ResponseEntity;
 
+import com.rdrg.back.dto.request.payment.PatchRentStatusResponseDto;
 import com.rdrg.back.dto.request.payment.PostPaymentRequestDto;
 import com.rdrg.back.dto.response.ResponseDto;
 import com.rdrg.back.dto.response.payment.GetPaymentDetailListResponseDto;
@@ -15,4 +16,5 @@ public interface PaymentService {
     ResponseEntity<? super GetPaymentListResponseDto> getPaymentList(String rentUserId);
     ResponseEntity<? super GetPaymentDetailListResponseDto> getPaymentDetailList(String rentUserId, int rentNumber);
     ResponseEntity<ResponseDto> deletePayment(int rentNumber);
+    ResponseEntity<ResponseDto> patchRentStatus(int rentNumber, PatchRentStatusResponseDto patchRentStatusResponseDto);
 }
