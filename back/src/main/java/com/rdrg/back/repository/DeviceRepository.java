a689dfdae68a7bcb3f757ed3a158001e3c04e29c
Chixol <phw9278@gmail.com>
2024-06-13 10:42:42 +0900

    예약 가능한 기기 리스트 불러오기 쿼리 메서드 수정

--- back/src/main/java/com/rdrg/back/repository/DeviceRepository.java
+++ back/src/main/java/com/rdrg/back/repository/DeviceRepository.java
@@ -28,12 +28,13 @@ public interface DeviceRepository extends JpaRepository<DeviceEntity, String> {
         @Query("SELECT ds FROM DevicesStatus ds WHERE ds.place = :inputPlace AND ds.serialNumber NOT IN " +
         "(SELECT rd.serialNumber FROM RentDetail rd WHERE rd.rentNumber IN " +
         "(SELECT drs.rentNumber FROM DeviceRentStatus drs WHERE " +
-        "(drs.rentDatetime < :inputRentDatetime AND drs.rentReturnDatetime > :inputReturnDatetime) OR " +
-        "(drs.rentDatetime < :inputReturnDatetime AND drs.rentReturnDatetime > :inputReturnDatetime) OR " +
-        "(drs.rentDatetime > :inputRentDatetime AND drs.rentReturnDatetime < :inputReturnDatetime)))")
+        "(drs.rentDatetime < :inputRentDatetime AND drs.rentReturnDatetime  > :inputRentReturnDatetime) OR " +
+        "(drs.rentDatetime < :inputReturnDatetime AND drs.rentReturnDatetime  > :inputRentReturnDatetime) OR " +
+        "(drs.rentDatetime > :inputRentDatetime AND drs.rentReturnDatetime  < :inputRentReturnDatetime)))")
 List<DeviceEntity> findAvailableDevices(
     @Param("inputRentDatetime") String inputRentDatetime,
     @Param("inputReturnDatetime") String inputReturnDatetime,
+    @Param("inputRentReturnDatetime") String inputRentReturnDatetime,
     @Param("inputPlace") String inputPlace);
 
     // @Query("SELECT ds FROM DevicesStatus ds " +
