350101329076835605466c1235b1886acb864eaf
Chixol <phw9278@gmail.com>
2024-06-17 14:09:26 +0900

    게시물 작성, 수정 첨부파일 오리지널 이름표시 추가

--- back/src/main/java/com/rdrg/back/dto/request/board/PostBoardRequestDto.java
+++ back/src/main/java/com/rdrg/back/dto/request/board/PostBoardRequestDto.java
@@ -2,6 +2,8 @@ package com.rdrg.back.dto.request.board;
 
 import java.util.List;
 
+import com.rdrg.back.common.object.BoardFileItem;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -11,12 +13,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class    PostBoardRequestDto {
+public class PostBoardRequestDto {
     @NotBlank
     private String title;
     @NotBlank
     private String contents;
     @NotNull
-    private List<String> urlList;
-    private List<String> originalFileNameList;
+    private List<BoardFileItem> fileList;
 }
